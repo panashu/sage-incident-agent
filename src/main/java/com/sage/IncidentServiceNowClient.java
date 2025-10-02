981c6558c1a13d461449ac3fc3d72160bb637e53
Ashutosh Pandey <57156180+panashu@users.noreply.github.com>
2025-10-02 22:31:47 +0200

    Create IncidentServiceNowClient.java

---
+++ src/main/java/com/sage/IncidentServiceNowClient.java
@@ -0,0 +1,26 @@
+package com.sage;
+
+import org.apache.http.client.methods.*;
+import org.apache.http.impl.client.*;
+import org.apache.http.entity.*;
+import org.apache.http.util.EntityUtils;
+
+public class IncidentServiceNowClient {
+    public static void createIncident(Diagnosis diagnosis, String solution) {
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
+            HttpPost request = new HttpPost("<SERVICENOW_INSTANCE>/api/now/table/incident");
+            String json = "{ \"short_description\": \"" + diagnosis.getIssue() +
+                          "\", \"description\": \"" + diagnosis.getDetails() +
+                          "\\nProbable fix: " + solution + "\" }";
+
+            request.setEntity(new StringEntity(json));
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Authorization", "Basic <BASE64_CREDENTIALS>");
+
+            CloseableHttpResponse response = client.execute(request);
+            System.out.println(EntityUtils.toString(response.getEntity()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
