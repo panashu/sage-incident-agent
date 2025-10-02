f394808f512bc35d30d5f13b2f40cc9b36e5523b
Ashutosh Pandey <57156180+panashu@users.noreply.github.com>
2025-10-02 22:29:44 +0200

    Create LogFetcher.java

---
+++ src/main/java/com/sage/LogFetcher.java
@@ -0,0 +1,17 @@
+package com.sage;
+
+import com.azure.monitor.query.*;
+import com.azure.monitor.query.models.*;
+
+public class LogFetcher {
+    public static String fetchRecentErrors() {
+        LogsQueryClient client = new LogsQueryClientBuilder()
+            .credential(new DefaultAzureCredentialBuilder().build())
+            .buildClient();
+
+        String query = "AppTraces | where SeverityLevel >= 3 | top 20 by TimeGenerated desc";
+        LogsQueryResult result = client.queryWorkspace("<LOG_ANALYTICS_WORKSPACE_ID>", query, QueryTimeInterval.LAST_5_MINUTES);
+
+        return result.getAllTables().toString();
+    }
+}
